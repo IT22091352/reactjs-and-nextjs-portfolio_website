6283847ca9b74b39ac46cfdcd2105325b62ad0d5
chathuka dilakshana <it22091352@my.sliit.lk>
2024-11-22 23:16:03 +0530

    chage

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,118 @@
+import Head  from "next/head";
 import Image from "next/image";
+import { LuSunMoon } from "react-icons/lu";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
+import { MdOutgoingMail } from "react-icons/md";
+
+
+//Image
+import profileImage from "../public/p2.png";
+import graphdesign from '../public/design.png';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <Head>
+        <title>
+          Portfolio Website
+        </title>
+          <style>
+             @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;800&display=swap');
+          </style>
+      </Head>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      <main className="font-poppins bg-yellow-50 min-h-screen px-10">
+        {/* nav nd gero */}
+        <section>
+          {/* nav */}
+          <nav className="py-10 flex justify-between">
+            <h1 className="text-sm">Developed by Chathuka Dilakshana</h1>
+            <ul className="flex items-center">
+              {/* icon */}
+               
+              <li ><LuSunMoon className="cursor-pointer text-3xl mx-5" /></li>
+              <li><a href="https://drive.google.com/file/d/16yY3D4lm8wYl70TnbJG8JQHVk6Nuowtj/view?usp=sharing" className="bg-yellow-400 text-black px-4 py-2 border-none rounded-md" download={"Resume"}>Resume</a></li>
+            </ul>
+          </nav>
+
+          {/* hero */}
+          
+          <div className="text-center p-10">
+            <h1 className="text-5xl font-bold text-yellow-400">My Projects</h1>
+            <h3 className="text-2xl py-4">Mobile App and Website Developer</h3>
+            <p className="text-sm py-4 text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+               ipsum, dolor distinctio incidunt alias
+               facilis fugit excepturi. Ab quae perspiciatis, 
+               maxime commodi a iure corporis accusamus amet consectetur non. Commodi.</p>
+
+          </div>
+
+          {/* social media icons */}
+          <div className="text-4xl flex justify-center gap-16 text-gray-600">
+            <a href="https://www.linkedin.com/in/chathuka-dilakshana-006315284/" ><FaLinkedin /></a>
+            <a href="https://github.com/IT22091352" ><FaGithubSquare /></a>
+            <a href="chathukadilakshana33@gmail.com" ><MdOutgoingMail /></a>
+          </div>
+
+          {/* profile */}
+          <div className="m-auto relative bg-gradient-to-b from-yellow-200 w-80 h-80 p-5 mt-20 rounded-full">
+            <Image 
+            src={profileImage}
+            alt={"profile picture"} 
+            layout="fill" 
+            objectFit="cover"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+          </div>
+        </section>
+
+
+        {/* Services session */}
+        <section>
+          <div>
+            <h2>Services and Offers</h2>
+            <p className="text-sm py-4 text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+               ipsum, dolor distinctio incidunt alias
+               facilis fugit excepturi. Ab quae perspiciatis, 
+               maxime commodi a iure corporis accusamus amet consectetur non. Commodi.</p>
+          </div>
+
+          {/* services card */}
+          <div>
+            {/* card1 */}
+            <div>
+              <Image src={graphdesign}
+               alt="graph designer"
+                width={100}
+            />
+            <h2 className="">Graphic Designing</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+               Quia dolores praesentium dolorum corrupti incidunt aliquam 
+               iste beatae! Nam, animi. Quod recusandae quibusdam 
+              accusamus explicabo sed. Officia, doloribus! Ipsa,
+               corporis architecto!</p>
+            </div>
+
+            
+           {/* card1 */}
+           <div></div>
+
+           {/* card1 */}
+           <div></div>
+
+
+
+          </div>
+        </section>
+
+
+
+
+
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      
+      
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
+export const content = [
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+  // Or if using `src` directory:
+  "./src/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
   },
-  plugins: [],
-}
+};
+export const plugins = [];
